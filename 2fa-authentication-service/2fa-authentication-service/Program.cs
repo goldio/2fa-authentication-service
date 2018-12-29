fe51f445014cd7502bd438cb96b55dedeba29374
Profiteam <31069986+Profiteam@users.noreply.github.com>
2018-12-29 17:16:54 +0300

    update

--- 2fa-authentication-service/2fa-authentication-service/Program.cs
+++ 2fa-authentication-service/2fa-authentication-service/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace _2fa_authentication_service
+namespace Gold.IO.Authentication.Service
 {
     public class Program
     {
--- 2fa-authentication-service/2fa-authentication-service/Startup.cs
+++ 2fa-authentication-service/2fa-authentication-service/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace _2fa_authentication_service
+namespace Gold.IO.Authentication.Service
 {
     public class Startup
     {
